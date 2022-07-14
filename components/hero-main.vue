914f39def09bac9dce11c433598aa8e3fdb37028
Pulkit Singh <44489787+linktotheart@users.noreply.github.com>
2022-07-14 13:16:08 +0530

    added changes, pages, theme supoort

--- components/hero-main.vue
+++ components/hero-main.vue
@@ -1,40 +1,34 @@
 <template>
-  <div class="hero min-h-screen  bg-base-200">
-    <div class="hero-content container mx-auto flex-col w-full justify-between lg:flex-row-reverse">
-		<div class="flex items-center w-60 bg-base-300 h-60 rounded-full">
-			<nuxt-img src="/img/pulkit (1).png" />
-		</div>
+  <div class="hero min-h-screen bg-base-200">
+    <div
+      class="hero-content container mx-auto flex-col w-full justify-between lg:flex-row-reverse"
+    >
+      <div class="flex items-center w-60 bg-base-300 h-60 rounded-full">
+        <nuxt-img src="/img/pulkit (1).png" />
+      </div>
 
       <div class="max-w-lg flex flex-col items-center lg:items-start">
-        <div class="badge">
-          Hey!
-        </div>
+        <div class="badge">Hey!!!!</div>
         <h1 class="text-5xl font-bold">
           I'm <span class="text-primary">Pulkit Singh</span>
         </h1>
         <p class="py-6 text-center lg:text-left">
-         I'm a Frontend developer & Art Director focused on digital design for brands that like to have fun.
+          I'm a Frontend developer & Art Director focused on digital design for
+          brands that like to have fun.
         </p>
-		<!-- <p class="text-sm text-gray-500 ">Working at Neotas</p> -->
-		<div class="flex space-x-4">
-			<button class="btn btn-primary">
-			  Say Hello
-			</button>
-			<button class="btn ">
-			  About Me
-			</button>
-		</div>
+        <!-- <p class="text-sm text-gray-500 ">Working at Neotas</p> -->
+        <div class="flex space-x-4">
+          <button class="btn btn-primary">Say Hello</button>
+          <button class="btn">About Me</button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {}
   },
   variants: {
-    extend: {},
+    extend: {}
   },
   plugins: [require("daisyui")],
   daisyui: {
     styled: true,
-    themes: ["synthwave", "light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "dracula", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+    themes: ["bumblebee"],
     base: true,
     utils: true,
     logs: true,
     rtl: false,
     prefix: "",
-    darkTheme: "dark",
-  },
-}
+    darkTheme: "dark"
+  }
+  // "light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "dracula", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"
+};
