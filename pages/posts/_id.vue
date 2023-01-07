99292a153ac41b7846e633c70329b67f6857a45a
Pulkit <pulkitsingh302@gmail.com>
2023-01-07 23:19:13 +0530

    header responsive

---
+++ pages/posts/_id.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+	  test-{{ $route.params.id   }}
+    fff {{posts}}
+	</div>
+  </template>
+
+  <script>
+  export default {
+    mounted: () => {
+      
+    },
+    async asyncData({ params }) {
+      const posts = await   queryContent('posts', params.id)
+        .only(['title'])
+        .sortBy('createdAt', 'asc')
+        .fetch()
+
+      return {
+        posts
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
